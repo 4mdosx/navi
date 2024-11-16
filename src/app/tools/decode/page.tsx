5244b28a786482ffb4fdacba6ce16f3fc6048052
4mdosx <4md.lain@gmail.com>
2024-11-16 16:39:05 +0800

    update deploy config

--- src/app/tools/decode/page.tsx
+++ src/app/tools/decode/page.tsx
@@ -90,7 +90,7 @@ export default function Decode() {
           ))}
         </div>
         <div>
-          {inputType} -> {targetType}
+          {inputType} -{'>'}` {targetType}
         </div>
       </main>
     </div>
