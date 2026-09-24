fa69c4f8f77285e159e21ef32bc0ccffc88c459c
4mdosx <4md.lain@gmail.com>
2026-09-24 23:32:13 +0800

    add force dynatic

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,8 @@ import { Suspense } from 'react'
 import { requireAppAccess } from '@/backstage/service/auth.service'
 import WeekPlanPage from './_todo/todo-center'
 
+export const dynamic = 'force-dynamic'
+
 export default async function HomePage() {
   await requireAppAccess()
   return (
--- app/settings/page.tsx
+++ app/settings/page.tsx
@@ -3,6 +3,8 @@ import { requireAppAccess } from '@/backstage/service/auth.service'
 import { getPinStatus } from '@/backstage/service/pin.service'
 import SettingsWorkspace from './settings-workspace'
 
+export const dynamic = 'force-dynamic'
+
 export const metadata = {
   title: '设置 · Navi',
 }
