200fbbbe5e6a3a92cc2b587bdb89d6a38919e76d
4mdosx <4md.lain@gmail.com>
2026-09-24 23:03:50 +0800

    udpate config

--- next.config.js
+++ next.config.js
@@ -6,6 +6,7 @@ const allowedOrigins = (process.env.SERVER_ACTION_ALLOWED_ORIGINS ?? '')
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: {
+    trustHostHeader: true, 
     serverActions: {
       allowedOrigins,
     },
